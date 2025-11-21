c9dbae06d66f2e0c9a5ab403d3d6530f78d0b51f
Marvan <marvansm@code.edu.az>
2025-11-21 20:10:47 +0400

    _

--- client/src/Featured/Sections/BrandsSection/index.tsx
+++ client/src/Featured/Sections/BrandsSection/index.tsx
@@ -1,8 +1,78 @@
+import Marquee from "react-fast-marquee";
 
 const BrandSection = () => {
   return (
-    <div>index</div>
-  )
-}
+    <section className="my-[72px]">
+      <Marquee speed={50} gradient={false} pauseOnHover={true}>
+        <div className="flex items-center gap-7">
+          <div>
+            <img
+              src="/Ellipse 51.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="/Ellipse 60.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="/Ellipse 61.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="/Ellipse 62.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
 
-export default BrandSection
+          <div>
+            <img
+              src="/Ellipse 64.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="/Ellipse 65.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="/Ellipse 61.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="/Ellipse 66.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+          <div>
+            <img
+              src="/Ellipse 67.png"
+              alt=""
+              className="w-[130px] h-[130px] object-cover"
+            />
+          </div>
+        </div>
+      </Marquee>
+    </section>
+  );
+};
+
+export default BrandSection;
