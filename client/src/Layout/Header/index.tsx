0dfa7e84f7e56aa7e99937913f23ad09cc7bf7f4
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-11-21 17:35:38 +0400

    _

--- client/src/Layout/Header/index.tsx
+++ client/src/Layout/Header/index.tsx
@@ -1,8 +1,26 @@
-
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="max-w-[1440px] flex items-center mx-auto container">
+      <div>
+        <img src="/Logo.png" alt="" />
+      </div>
+      <nav className="max-w-[1296px] flex items-center mx-auto container ">
+        <div className="flex justify-between max-w-[846px] items-center">
+          <ul className="flex gap-14 text-[24px] font-inter font-semibold">
+            <li>Home</li>
+            <li>About me</li>
+            <li>Services</li>
+            <li>Blog</li>
+            <li>FAQ</li>
+          </ul>
+          <button>Contact</button>
+        </div>
+      </nav>
+      <div>
+        <h5>sakfghgj</h5>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
