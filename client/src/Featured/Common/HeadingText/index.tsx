58b07a4b281f6c98d094391736b0224aab20d74f
Marvan <marvansm@code.edu.az>
2025-11-22 01:41:21 +0400

    _

--- client/src/Featured/Common/HeadingText/index.tsx
+++ client/src/Featured/Common/HeadingText/index.tsx
@@ -3,9 +3,9 @@ import type { headingTypes } from "../../../Types/global";
 const HeadingText = ({ title, }: headingTypes) => {
   return (
     <div>
-      <div className="mx-auto container  max-w-[1200px] py-[13px]">
+      <div className="mx-auto container  max-w-[1200px] ">
         <div className="flex items-center justify-center text-center">
-          <div className="flex  items-center pt-10">
+          <div className="flex  items-center ">
             <h2 className="text-[48px] leading-[55px] font-inter font-medium text-[#1B1B1B]">
               {title}  
             </h2>
--- client/src/Featured/Templates/HomeTemp.tsx
+++ client/src/Featured/Templates/HomeTemp.tsx
@@ -1,6 +1,7 @@
 import BannerSection from "../Sections/BannerSection";
 import BrandSection from "../Sections/BrandsSection";
 import PlanSections from "../Sections/PlanSections";
+import ReviewSection from "../Sections/ReviewSection";
 import ServiceSection from "../Sections/ServiceSection";
 
 const HomeTemp = () => {
@@ -10,6 +11,7 @@ const HomeTemp = () => {
       <BrandSection />
       <ServiceSection />
       <PlanSections />
+      <ReviewSection />
     </div>
   );
 };
