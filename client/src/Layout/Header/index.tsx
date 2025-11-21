78b13602eeb3e68586147e6618057876f1320c2f
Marvan <marvansm@code.edu.az>
2025-11-21 18:17:56 +0400

    _

--- client/src/Layout/Header/index.tsx
+++ client/src/Layout/Header/index.tsx
@@ -1,23 +1,48 @@
 const Header = () => {
   return (
-    <header className="max-w-[1440px] flex items-center mx-auto container">
+    <header className="max-w-[1296px] flex items-center justify-between mx-auto py-10 ">
       <div>
-        <img src="/Logo.png" alt="" />
+        <img
+          src="/Logo.png"
+          alt=""
+          className="w-[72px] h-[72px] object-contain"
+        />
       </div>
-      <nav className="max-w-[1296px] flex items-center mx-auto container ">
-        <div className="flex justify-between max-w-[846px] items-center">
-          <ul className="flex gap-14 text-[24px] font-inter font-semibold">
-            <li>Home</li>
-            <li>About me</li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>FAQ</li>
-          </ul>
-          <button>Contact</button>
-        </div>
+      <nav className=" w-[846px] flex items-center justify-between ">
+        <ul className="flex gap-14 text-[24px] font-inter font-semibold">
+          <li className="flex items-center justify-center border-b-[3px] border-b-transparent hover:border-b-[#E5D037] duration-300 cursor-pointer">
+            Home
+          </li>
+          <li className="flex items-center justify-center border-b-[3px] border-b-transparent hover:border-b-[#E5D037] duration-300 cursor-pointer">
+            About me
+          </li>
+          <li className="flex items-center justify-center border-b-[3px] border-b-transparent hover:border-b-[#E5D037] duration-300 cursor-pointer">
+            Services
+          </li>
+          <li className="flex items-center justify-center border-b-[3px] border-b-transparent hover:border-b-[#E5D037] duration-300 cursor-pointer">
+            Blog
+          </li>
+          <li className="flex items-center justify-center border-b-[3px] border-b-transparent hover:border-b-[#E5D037] duration-300 cursor-pointer">
+            FAQ
+          </li>
+        </ul>
+        <button className="px-3 py-2 bg-[#0707B0] text-white w-[150px] h-12 rounded-[50px] text-[16px] font-inter leading-6 font-normal">
+          Contact
+        </button>
       </nav>
       <div>
-        <h5>sakfghgj</h5>
+        <ul className="flex items-center gap-3 w-[97px] justify-center">
+          <li className="text-[24px] font-inter font-medium leading-[34px] text-black">
+            Az
+          </li>
+          <li>
+            <img
+              src="/Aze.png"
+              alt=""
+              className="w-[42px] h-[42px] object-contain"
+            />
+          </li>
+        </ul>
       </div>
     </header>
   );
